80df0d52983cc31c410caafd8a761388ed5d86dc
tkduncan <troy.duncan@my.utsa.edu>
2024-02-05 11:30:00 -0600

    example in int_lib now asks for user input. Separated int and double into separate folders

--- assignment2/int_lib/example.c
+++ assignment2/int_lib/example.c
@@ -2,8 +2,13 @@
 #include "int_arithmetic.h"
 
 int main(void) {
-    int num1 = 4;
-    int num2 = 5;
+    int num1 = 0;
+    int num2 = 0;
+
+    printf("Enter a value for num1:\n");
+    scanf("%d", &num1);
+    printf("Enter a value for num2:\n");
+    scanf("%d", &num2);
 
     printf("num1 + num2 = %d\n", add(num1, num2));
     printf("num1 - num2 = %d\n", subtract(num1, num2));
