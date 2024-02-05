dac8eba6b4ec6c0c0e3ed46f7b630bcf8c7326ba
tkduncan <troy.duncan@my.utsa.edu>
2024-02-04 23:49:21 -0600

    completed static library and example for ints in c

--- assignment2/example.c
+++ assignment2/example.c
@@ -6,6 +6,9 @@ int main(void) {
     int num2 = 5;
 
     printf("num1 + num2 = %d\n", add(num1, num2));
+    printf("num1 - num2 = %d\n", subtract(num1, num2));
+    printf("num1 * num2 = %d\n", multiply(num1, num2));
+    printf("num1 / num2 = %d\n", divide(num1, num2));
     
     return 0;
 }
--- assignment2/int_arithmetic.h
+++ assignment2/int_arithmetic.h
@@ -2,7 +2,7 @@
 #define INTARITHMETIC_H
 
 int add(int num1, int num2);
-int subctract(int num1, int num2);
+int subtract(int num1, int num2);
 int multiply(int num1, int num2);
 int divide(int num1, int num2);
 
