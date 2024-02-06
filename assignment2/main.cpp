8ef389997cd043b5a7a2191c75d9fed9965d032a
tkduncan <troy.duncan@my.utsa.edu>
2024-02-06 14:58:56 -0600

    now ignoring the Makefile in assignment2

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -9,12 +9,14 @@ int main(void)
     int num1 = 0, num2 = 0;
     double num3 = 0, num4 = 0;
     
+    // grab user input for each integer
     cout << "Enter an integer for num1: " << endl;
     cin >> num1;
 
     cout << "Enter an integer for num2: " << endl;
     cin >> num2;
-    // Make sure you're not dividing by 0 and is an int
+
+    // make sure you're not dividing by 0 and is an int
     while (num2 == 0)
     {
         cout << "Invalid. Cannot divide by 0" << endl;
@@ -23,18 +25,21 @@ int main(void)
     }
     cout << endl;
 
+    // run the integers through each function
     cout << "Using C static library:" << endl;
     cout << "num1 + num2 = " << add(num1, num2) << endl;
     cout << "num1 - num2 = " << subtract(num1, num2) << endl;
     cout << "num1 * num2 = " << multiply(num1, num2) << endl;
     cout << "num1 / num2 = " << divide(num1, num2) << endl << endl;
     
+    // grab user input for each double
     cout << "Enter a double for num3: " << endl;
     cin >> num3;
 
     cout << "Enter a double for num4: " << endl;
     cin >> num4;
-    // Make sure you're not dividing by 0
+
+    // make sure you're not dividing by 0
     while (num4 == 0)
     {
         cout << "Invalid entry. Can't divide by 0" << endl;
@@ -43,6 +48,7 @@ int main(void)
     }
     cout << endl;
 
+    // run the doubles through each function
     cout << "Using C++ static library:" << endl;
     cout << "num3 + num4 = " << addition(num3, num4) << endl;
     cout << "num3 - num4 = " << subtraction(num3, num4) << endl;
