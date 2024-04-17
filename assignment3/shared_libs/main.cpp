c98f18476ca7376d9e8535d08dea25f7fbec0548
tkduncan <troy.duncan@my.utsa.edu>
2024-04-17 15:06:16 -0500

    minor adjustments to shared_libs

--- assignment3/shared_libs/main.cpp
+++ assignment3/shared_libs/main.cpp
@@ -16,17 +16,17 @@ int main(void)
 
     // run the integers through each function
     cout << "Using C shared library:" << endl;
-    cout << "num1 + num2 = " << add(num1, num2) << endl;
-    cout << "num1 - num2 = " << subtract(num1, num2) << endl;
-    cout << "num1 * num2 = " << multiply(num1, num2) << endl;
-    cout << "num1 / num2 = " << divide(num1, num2) << endl << endl;
+    cout << num1 << " + " << num2 << " = " << add(num1, num2) << endl;
+    cout << num1 << " - " << num2 << " = " << subtract(num1, num2) << endl;
+    cout << num1 << " * " << num2 << " = " << multiply(num1, num2) << endl;
+    cout << num1 << " / " << num2 << " = " << divide(num1, num2) << endl << endl;
 
     // run the doubles through each function
     cout << "Using C++ shared library:" << endl;
-    cout << "num3 + num4 = " << addition(num3, num4) << endl;
-    cout << "num3 - num4 = " << subtraction(num3, num4) << endl;
-    cout << "num3 * num4 = " << multiplication(num3, num4) << endl;
-    cout << "num3 / num4 = " << division(num3, num4) << endl;
+    cout << num3 << " + " << num4 << " = " << addition(num3, num4) << endl;
+    cout << num3 << " - " << num4 << " = " << subtraction(num3, num4) << endl;
+    cout << num3 << " * " << num4 << " = " << multiplication(num3, num4) << endl;
+    cout << num3 << " / " << num4 << " = " << division(num3, num4) << endl;
 
     return 0;
 }
